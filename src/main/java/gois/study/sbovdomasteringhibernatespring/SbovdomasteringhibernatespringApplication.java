dfad005783c3879cda9937954bbb0d9f1293ab8a
Gustavo Gois <gustavo.gois@gmail.com>
2019-11-29 22:42:30 +0000

    Initial commit

---
+++ src/main/java/gois/study/sbovdomasteringhibernatespring/SbovdomasteringhibernatespringApplication.java
@@ -0,0 +1,13 @@
+package gois.study.sbovdomasteringhibernatespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbovdomasteringhibernatespringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbovdomasteringhibernatespringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gois/study/sbovdomasteringhibernatespring/SbovdomasteringhibernatespringApplicationTests.java
@@ -0,0 +1,13 @@
+package gois.study.sbovdomasteringhibernatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbovdomasteringhibernatespringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
