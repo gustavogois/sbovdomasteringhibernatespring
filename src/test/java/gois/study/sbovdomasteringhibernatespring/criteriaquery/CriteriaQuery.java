d8f6d00981d5df7c942940e689e762228d5f219c
Gustavo Gois <gustavo.gois@gmail.com>
2019-12-03 22:54:35 +0000

    Closes #7

--- src/test/java/gois/study/sbovdomasteringhibernatespring/criteriaquery/CriteriaQuery.java
+++ src/test/java/gois/study/sbovdomasteringhibernatespring/criteriaquery/CriteriaQuery.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
@@ -23,7 +24,7 @@ public class CriteriaQuery {
     EntityManager em;
 
     @Test
-    public void name() {
+    public void allCourses() {
 
         // Use Criteria Builder to create a Criteria Query returning the expected result object
         CriteriaBuilder cb = em.getCriteriaBuilder();
@@ -42,4 +43,28 @@ public class CriteriaQuery {
 
         log.info("Typed Query: {}", resultList);
     }
+
+    @Test
+    public void allCoursesHaving100Steps() {
+
+        // Use Criteria Builder to create a Criteria Query returning the expected result object
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        javax.persistence.criteria.CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+        // Define roots for tables which are involved in the Query
+        Root<Course> courseRoot = cq.from(Course.class);
+
+        // Define Predicates etc using Criteria Builder
+        Predicate like100Steps = cb.like(courseRoot.get("name"), "%100 Steps");
+
+        // Add Predicates etc to the Criteria Query
+        cq.where(like100Steps);
+
+        // Build the TypedQuery using the entity manager and criteria query
+        TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+        List<Course> resultList = query.getResultList();
+
+        log.info("Typed Query: {}", resultList);
+    }
+
 }
